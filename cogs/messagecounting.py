6dbab7826745e7ce0ade90d5c97b4dfe2c482cf7
alexx2021 <78677978+alexx2021@users.noreply.github.com>
2022-10-28 12:18:26 -0700

    current state

--- cogs/messagecounting.py
+++ cogs/messagecounting.py
@@ -1,3 +1,4 @@
+from wsgiref.simple_server import server_version
 from discord.ext import commands
 import discord
 
@@ -5,11 +6,11 @@ class MessageCounting(commands.Cog, command_attrs=dict(hidden=True)):
     def __init__(self, bot):
         self.bot = bot
         self.cd_mapping = commands.CooldownMapping.from_cooldown(
-            1, 10, commands.BucketType.member
+            1, 1, commands.BucketType.member
         )
 
     @commands.Cog.listener()
-    async def on_message(self, message):
+    async def on_message(self, message: discord.Message):
         await self.bot.wait_until_ready()
 
         if message.author == self.bot.user:
@@ -24,24 +25,35 @@ class MessageCounting(commands.Cog, command_attrs=dict(hidden=True)):
         bucket = self.cd_mapping.get_bucket(message)
         retry_after = bucket.update_rate_limit()
         if not retry_after:
-            ongoingGame = await self.bot.db.execute('SELECT * FROM guild_config WHERE guild_id = ?',(message.guild.id,))
-            ongoingGame = await ongoingGame.fetchone()
-            if str(ongoingGame[1]) == 'True':
-                existingPlayer = await self.bot.db.execute('SELECT user_id, message_count FROM messagecount WHERE guild_id = ? AND user_id = ?',(message.guild.id, message.author.id))
-                existingPlayer = await existingPlayer.fetchone()
-                if not existingPlayer:
-                    await self.bot.db.execute('INSERT INTO messagecount VALUES (?, ?, ?)',(message.guild.id, message.author.id, 1))
-                    await self.bot.db.commit()
-                else:
-                    await self.bot.db.execute('UPDATE messagecount SET message_count = ? WHERE guild_id = ? AND user_id = ?', (existingPlayer[1] + 1, message.guild.id, message.author.id))
-                    await self.bot.db.commit()
+            serverConfig = await self.bot.db.execute('SELECT * FROM guild_config WHERE guild_id = ?',(message.guild.id,))
+            serverConfig = await serverConfig.fetchone()
+            print("===============================")
+            if serverConfig: #if the guild has a config set
+                print("guild has config set")
+                if str(serverConfig[1]) == 'True': #ongoing game
+                    print("guild has ongoing game")
+                    if str(serverConfig[2]) in str(message.author.roles): #if user has player role
+                        print("PLayer has player role")
+                        if not (str(serverConfig[3]) in str(message.author.roles)): #if loser role not in player roles
+                            print("doesnt have loser role DONE")
+                            charRequirement = serverConfig[4]
+                            wordRequirement = serverConfig[5]
+                            if len(message.clean_content) >= charRequirement: #check char requirement
+                                print("passed char req")
+                                splitString = message.clean_content.split()
+                                if len(splitString) >= wordRequirement:
+                                    print("passed word req")
+                                    existingPlayer = await self.bot.db.execute('SELECT user_id, message_count FROM messagecount WHERE guild_id = ? AND user_id = ?',(message.guild.id, message.author.id))
+                                    existingPlayer = await existingPlayer.fetchone()
+                                    if not existingPlayer:
+                                        await self.bot.db.execute('INSERT INTO messagecount VALUES (?, ?, ?)',(message.guild.id, message.author.id, 1))
+                                        await self.bot.db.commit()
+                                    else:
+                                        await self.bot.db.execute('UPDATE messagecount SET message_count = ? WHERE guild_id = ? AND user_id = ?', (existingPlayer[1] + 1, message.guild.id, message.author.id))
+                                        await self.bot.db.commit()
 
-        # TODO add out role+in role to qualify for xp
 
         
-
-
-
 
 async def setup(bot):
     await bot.add_cog(MessageCounting(bot)) 
